1742b6a6679a1204f3c5e1bf933d8926590bdd45
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-10-30 17:55:57 -0700

    validations and rspec tests

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,6 @@
 class Member < ActiveRecord::Base
   has_one :feed
+  validates :username, :password, presence: true
+  validates :username, uniqueness: true
+  validates :username, :password, length: { in: 6..20 }
 end
