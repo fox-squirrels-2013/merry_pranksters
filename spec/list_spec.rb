dbca221e6677215b43e4ca405e067d296f5236b2
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-10-30 18:14:13 -0700

    added list spec, had to add to sign up view in order to have id's in the form so it would submit the test

---
+++ spec/list_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+feature "Listing members" do
+
+  scenario "User can see the list of members" do
+    visit '/sign_up'
+    fill_in "username", with: "bibble"
+    fill_in "password", with: "password"
+    click_on 'Save'
+    visit '/list'
+
+    expect(page).to have_content("list of merry pranksters")
+  end
+
+  # scenario "Visitor cannot see the list of members" do
+  #   visit '/'
+  #   # click_on 'Log Out' # if test is logged in -- does this even matter?
+  #   visit '/list'
+
+  #   expect(page).to have_content("Please log in to see a list of members")
+  end
+
+end
