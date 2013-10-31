39316ac4260b8d193a69c1c27cfa30f3e4b4e619
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-10-31 16:21:57 -0700

    added email to members table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131029234347) do
+ActiveRecord::Schema.define(version: 20131031231942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20131029234347) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
   create_table "posts", force: true do |t|
