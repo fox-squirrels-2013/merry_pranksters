d5d7f6a6ff90585ea27d3851ce5281510ed2d4be
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-01 18:28:57 -0700

    Adding a feed view, because it needs to be per member.

--- seed.rb
+++ seed.rb
@@ -8,7 +8,8 @@ ActiveRecord::Base.establish_connection(adapter: 'postgresql',
                                         database: 'social_network')
 
 60.times do
-  Member.create!(username: Faker::Name.first_name, password_hash: Faker::Internet.password)
+  mem = Member.create!(username: Faker::Name.first_name, password: Faker::Internet.password)
+  Feed.create!(member_id: mem.id)
 end
 
 100.times do
